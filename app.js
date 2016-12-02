a385519757c2752462a3eded3e25b34bcfd46e15
Waseem Ahmed <alphawaseem@gmail.com>
2016-12-02 18:01:27 +0530

    change zoom

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function showError(error) {
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center:city,
-    zoom: 16,
+    zoom: 13,
     styles: [{
       stylers: [{ visibility: 'simplified' }]
     }, {
